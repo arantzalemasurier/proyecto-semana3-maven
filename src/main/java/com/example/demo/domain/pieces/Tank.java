2bb80bc8edf3c3501ff12d0c4cac3372eb098e5b
arant <arant@LAPTOP-52PJ1EL5>
2021-08-07 23:03:22 +0200

    Arreglo pieza tank

--- src/main/java/com/example/demo/domain/pieces/Tank.java
+++ src/main/java/com/example/demo/domain/pieces/Tank.java
@@ -9,18 +9,21 @@ public class Tank {
 		this.id = id;
 		this.capacity = capacity;
 	}
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public Double getCapacity() {
 		return capacity;
 	}
+	
 	public void setId(Long id) {
-		this.id = id;
 	}
+	
 	public void setCapacity(Double capacity) {
-		this.capacity = capacity;
 	}
+	
 	@Override
 	public String toString() {
 		return "Battery [id=" + id + ", capacity=" + capacity + "]";
